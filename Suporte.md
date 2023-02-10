d758e9ed76482e040be86752d66905b3a4bab645
Guilherme Araujo <40188385+guilhermearaujo14@users.noreply.github.com>
2023-02-09 23:56:33 -0300

    Add files via upload
    Mudança no front

---
+++ Suporte.md
@@ -0,0 +1,3 @@
+#### COMPONENTES INSTALADOS PARA O PROJETO
+# 1 instalação do REACT NAVIGATION - Navegação de telas
+# 2 AXIOS - Utilização de APIs
---
+++ styleColors.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const colors = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#5838A0',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10
+      },
+    statusBar:{
+        backgroundColor: '#5838A0',
+        color: '#fff',
+    },
+})
+export default colors;
